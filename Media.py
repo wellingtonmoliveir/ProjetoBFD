b5846589d3560131ad6bb8afd7d250f3ec7c7f6b
wellingtonqualitec <wellingtonoliveira@gmail.com>
2025-11-05 10:27:22 -0300

    Inicio do código de média

---
+++ Media.py
@@ -0,0 +1,6 @@
+
+nomeAluno = input("Digite o nome do aluno: ")
+nota1 = float(input("Digite a 1 nota: "))
+nota2 = float(input("Digite a 2 nota: "))
+media = (nota1+nota2)/2
+print(f"olá {nomeAluno}, sua média é {media} e vc foi {estado}")
